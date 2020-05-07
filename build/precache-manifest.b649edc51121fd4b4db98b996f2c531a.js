3026d4c5b533b216b0fa31d1115f1640cf28bbcf
Danill Yudhistira Setiawan <danillyudhistira@gmail.com>
2020-05-07 12:26:42 +0700

    fixed gitignore

---
+++ build/precache-manifest.b649edc51121fd4b4db98b996f2c531a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef029536795bdab09319e38e6abf5d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21316a85dc0eada2a754",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ab9ec2244773ab1d67da",
+    "url": "/static/css/main.aac263d3.chunk.css"
+  },
+  {
+    "revision": "21316a85dc0eada2a754",
+    "url": "/static/js/2.2c286ee2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2c286ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9ec2244773ab1d67da",
+    "url": "/static/js/main.bd863fe5.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "e420a7fa1a98ede3d16d9895338a6f0f",
+    "url": "/static/media/avatar.e420a7fa.jpg"
+  }
+]);
